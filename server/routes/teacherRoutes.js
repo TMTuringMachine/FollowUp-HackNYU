81c097cc3fa3489d3b119dfd8badda062e463a50
AryanShinde <aryan.shinde20@vit.edu>
2022-02-26 22:58:07 +0530

    middleware added

--- server/routes/teacherRoutes.js
+++ server/routes/teacherRoutes.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const { isTeacher } = require("../middlewares/isTeacher");
+
 const {
   signup,
   login,
   jwtVerify,
   createClass,
   addSubject,
-  addTest
+  addTest,
 } = require("../controllers/teachers");
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.get("/jwtVerify", jwtVerify);
-router.post("/createClass", createClass);
-router.post("/addSubject", addSubject);
-router.post("/addTest",addTest)
-
+router.post("/createClass", isTeacher, createClass);
+router.post("/addSubject", isTeacher, addSubject);
+router.post("/addTest", isTeacher, addTest);
 
 module.exports = router;
