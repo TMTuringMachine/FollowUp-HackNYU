4296ec6f659be00b920c351d9fb63cb72c0f6929
AryanShinde <aryan.shinde20@vit.edu>
2022-03-03 01:55:27 +0530

    to the prod

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
-
+const path = require("path");
 //using middleware to parse json data
 
 app.use(express.json());
@@ -13,6 +13,12 @@ app.use(cors());
 require("./db/conn");
 app.use("/teacher", require("./routes/teacherRoutes"));
 app.use("/student", require("./routes/studentRoutes"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`App listening on port http://localhost:${port}`);
