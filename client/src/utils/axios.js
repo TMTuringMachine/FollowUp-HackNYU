a841872a1af2d347fe4f4313ce4e10aab188b3bd
AryanShinde <aryan.shinde20@vit.edu>
2022-03-03 15:59:40 +0530

    work in prod

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
-
+import dotenv from "dotenv";
+dotenv.config({ path: "../../config.env" });
+const port = process.env.PORT || 5000;
 const axiosInstance = axios.create({
-  baseURL: `http://localhost:5000`,
+  baseURL: `http://localhost:${port}`,
 });
 
 export default axiosInstance;
