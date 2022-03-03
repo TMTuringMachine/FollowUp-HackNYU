beb86e54e8a72e191fd0343558de3bc6e81e50f7
AryanShinde <aryan.shinde20@vit.edu>
2022-03-03 15:18:52 +0530

    work in prod

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const path = require("path");
+const port = process.env.PORT || 5000;
+
 //using middleware to parse json data
 
 app.use(express.json());
