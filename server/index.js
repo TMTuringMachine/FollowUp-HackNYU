4bf2d060b050fb0f483c569da2c94beab69108df
AryanShinde <aryan.shinde20@vit.edu>
2022-03-03 13:17:45 +0530

    work in prod

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV == "production") {
   console.log("here in prod");
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
 
